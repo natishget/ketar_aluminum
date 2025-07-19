aff10cc44c7680f22b9f7b038b9bc13e6291e5f3
cook-iess <ekramsiraj32@gmail.com>
2025-07-19 11:18:03 +0300

    change the phone number

--- app/products/ContactUsButtons.tsx
+++ app/products/ContactUsButtons.tsx
@@ -18,14 +18,14 @@ export default function ContactUsButtons() {
         Contact Us
       </Button>
 
-      <a href="tel:+251912345678">
+      <a href="tel:+251930329599">
         <Button
           size="lg"
           variant="outline"
           className="border-2 border-white text-white hover:bg-white hover:text-green-600 shadow-2xl bg-transparent px-10 py-4 text-lg font-semibold"
         >
           <PhoneCall className="mr-3 h-6 w-6" />
-          Call: +251 912 345 678
+          Call: +251 930 329599
         </Button>
       </a>
     </div>
